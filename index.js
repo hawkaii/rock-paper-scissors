56cdc12ddbf3c9d1c49f03a75d298933de8de33b
Parthib Mukherjee <parthibmukherjee@gmail.com>
2023-03-12 12:34:05 +0530

    finished and also added style

--- index.js
+++ index.js
@@ -4,61 +4,90 @@
 const playerText = document.querySelector("#playerText");
 const computerText = document.querySelector("#computerText");
 const resultText = document.querySelector("#resultText");
-const choiceBtn = document.querySelector(".choiceBtn");
-
-
-
-
-
-
+const buttons = document.querySelectorAll(".choiceBtn");
+const reset = document.querySelector(".Reset");
+const playerScore = document.querySelector("#playerScore");
+const computerScore = document.querySelector("#computerScore");
 
 let playerSelection;
-
 let computerSelection;
-//console.log(playRound(playerSelection, computerSelection));
 let result;
+let count = 0;
 
+let player = 0;
+let computer = 0;
 
+function startGame(){
+  count =0;
+  player = 0;
+  computer = 0;
+  playerText.textContent = `Player: `;
+  computerText.textContent = `Computer:`
+  resultText.textContent = ``;
+  playerScore.textContent = `Player: ${player}` ;
+  computerScore.textContent = `Computer: ${computer}`;
 
-game();
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        playerSelection = prompt("Do you choose rock,paper or scissors");
-        playerSelection = playerSelection.toLowerCase();
-        getComputerChoice();
-        console.log(playRound(playerSelection,computerSelection));
-
-    }
 }
 
-function getComputerChoice() {
+   reset.addEventListener('click',startGame)
+
+    buttons.forEach(btn => {
+      btn.addEventListener('click', () => {
+       if(count < 5) {
+         playerSelection = btn.value;
+         getComputerChoice();
+
+         playerText.textContent = `Player: ${playerSelection.toUpperCase()}`;
+         computerText.textContent = `Computer: ${computerSelection.toUpperCase()}`
+         result = (playRound(playerSelection, computerSelection));
+         resultText.textContent = `${result}`;
+         count++;
+       }
+      })
+    });
+
+
+
+  function getComputerChoice() {
     const ranNum = Math.floor(Math.random() * 3 + 1);
 
     if (ranNum === 1) {
-        computerSelection = "rock"
+      computerSelection = "rock";
     } else if (ranNum === 2) {
-        computerSelection = "scissors"
+      computerSelection = "scissors";
     } else if (ranNum === 3) {
-        computerSelection = "paper"
+      computerSelection = "paper";
     }
+  }
+
+  function playerWin() {
+      player++;
+      playerScore.textContent = `Player: ${player}` ;
+      return "You Win";
+
+  }
+  function computerWin() {
+      computer ++;
+      computerScore.textContent = `Computer: ${computer}`;
+      return "You Lose"
+  }
 
 
-}
-
-function playRound(playerSelection, computerSelection) {
-
+ function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        return "it's a draw";
-
+      return "it's a draw";
     } else if (computerSelection === "rock") {
-        return (playerSelection === "paper") ? "You Win! cause paper beats rock" : "You Lose cause rock beats scissors"
-
+      return playerSelection === "paper"
+          ? playerWin() +" cause paper beats rock"
+          : computerWin()+" cause rock beats scissors";
     } else if (computerSelection === "paper") {
-        return (playerSelection === "scissors") ? "You Win! cause scissors beats paper" : "You Lose cause paper beats rock"
+      return playerSelection === "scissors"
+          ? playerWin() +" cause scissors beats paper"
+          : computerWin()+" cause paper beats rock";
     } else if (computerSelection === "scissors") {
-        return (playerSelection === "rock") ? "You Win! cause rock beats scissors" : "You Lose cause scissors beats paper"
+      return playerSelection === "rock"
+          ? playerWin() +"  cause rock beats scissors"
+          : computerWin()+" scissors beats paper";
     }
-}
-
+  }
 
